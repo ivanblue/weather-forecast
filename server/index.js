887167be019a3fda4d24e9cfdfe10586893b3b05
IvanBlue <ivangarcete@gmail.com>
2023-02-06 22:38:49 -0300

    changes: - client: improved error handling. Added proxy route. Deleted unused images - server: implemented a generic proxy server in NodeJs

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const { default: axios } = require('axios');
+const express = require('express');
+const app = express();
+
+const PORT = 3001;
+
+app.get('/proxy/*', (req, res) => {
+  const url = req.path.replace('/proxy/', '');
+  const queryString = require('querystring').stringify(req.query);
+
+  console.log(`proxy'd url: ${url}?${queryString}`);
+
+  axios
+    .get(`${url}?${queryString}`)
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.listen(3001, function () {
+  console.log(`Proxy-server listening on port ${PORT}`);
+});
